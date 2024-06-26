02b6332d7197d6469ac3221f5e48cc18569c9de2
Anthony Monforte <amonforte@gmail.com>
2024-06-26 11:18:00 -0700

    Adding rudimentary image extraction

--- chunk_pdf.py
+++ chunk_pdf.py
@@ -1,6 +1,9 @@
 import argparse
 from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import fitz
+import os
+from tqdm import tqdm
 
 def main():
 
@@ -10,18 +13,30 @@ def main():
 
     print(args.p)
 
-    doc_loader = PyPDFDirectoryLoader(args.p)
-    docs = doc_loader.load()
+    # doc_loader = PyPDFDirectoryLoader(args.p)
+    # docs = doc_loader.load()
     
-    chunkifier = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=80,
-        length_function=len,
-        is_separator_regex=False
-    )
+    # chunkifier = RecursiveCharacterTextSplitter(
+    #     chunk_size=800,
+    #     chunk_overlap=80,
+    #     length_function=len,
+    #     is_separator_regex=False
+    # )
 
-    chunks = chunkifier.split_documents(docs)
-    print("Chunks: ", len(chunks))
+    # chunks = chunkifier.split_documents(docs)
+    # print("Chunks: ", len(chunks))
+
+    for path in os.listdir(args.p):        
+        if ".pdf" in path:
+            print(path)
+            doc = fitz.Document(os.path.join(args.p, path))
+
+            for page_num in tqdm(range(len(doc)), desc="pages"):
+                for img in doc.get_page_images(page_num):
+                    xref =img[0]
+                    image = doc.extract_image(xref)
+                    pix = fitz.Pixmap(doc, xref)
+                    pix.save(os.path.join(args.p, "images/" "%s_p%s-%s.png" % (path[:-4], page_num, xref)))
 
 if __name__ == "__main__":
     main()
