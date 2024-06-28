87eeb5e6855c924f052dd6107724237815de4a49
Anthony Monforte <amonforte@gmail.com>
2024-06-28 12:57:52 -0700

    Small refactor and path fix bug fix

--- chunk_pdf.py
+++ chunk_pdf.py
@@ -12,36 +12,39 @@ def main():
     arg_parser.add_argument("-p", required=True, help="PDF folder")
     args = arg_parser.parse_args()
 
-    print(args.p)
+    print(args.p)   
 
-    # doc_loader = PyPDFDirectoryLoader(args.p)
-    # docs = doc_loader.load()
-    
-    # chunkifier = RecursiveCharacterTextSplitter(
-    #     chunk_size=800,
-    #     chunk_overlap=80,
-    #     length_function=len,
-    #     is_separator_regex=False
-    # )
+    #extract_text_chunks(args.p)
+    extract_images(args.p)
 
-    # chunks = chunkifier.split_documents(docs)
-    # print("Chunks: ", len(chunks))
+def extract_text_chunks(folder_path):
+    doc_loader = PyPDFDirectoryLoader(folder_path)
+    docs = doc_loader.load()
+    chunkifier = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False
+    )
 
-    for path in os.listdir(args.p):        
-        if ".pdf" in path:
+    chunks = chunkifier.split_documents(docs)
+    print("Chunks: ", len(chunks))
+
+
+def extract_images(folder_path):
+    for path in os.listdir(folder_path):        
+        if path.endswith(".pdf"):
             print(path)
-            doc = fitz.Document(os.path.join(args.p, path))
-            fig_captions = []
-
+            doc = fitz.Document(os.path.join(folder_path, path))
+            
             for page_num in tqdm(range(len(doc)), desc="pages"):
                 img_num = 0
                 img_xrefs = []
 
                 for img in doc.get_page_images(page_num):
-                    xref =img[0]
-                    #image = doc.extract_image(xref)
+                    xref =img[0]                    
                     pix = fitz.Pixmap(doc, xref)
-                    pix.save(os.path.join(args.p, "images/" "%s_p%s-%s.png" % (path[:-4], page_num, xref)))
+                    pix.save(os.path.join(folder_path, "images/" "%s_p%s-%s.png" % (path[:-4], page_num, xref)))
                     img_num += 1
                     img_xrefs.append(xref)
 
@@ -51,17 +54,18 @@ def main():
 
                     page = doc.load_page(page_num)
                     text = page.get_text("text")
-                    #text_blocks = text.split('\n\n')
                     
-                    #for block in text_blocks:
                     for match in pattern.findall(text):
                         caption_num = 0                        
-                        #if pattern.search(block):                            
-                        path = os.path.join(args.p, "images/" "%s_p%s-%s.txt" % (path[:-4], page_num, img_xrefs[caption_num]))
-                        with open(path, "w") as file:
+                        
+                        caption_path = os.path.join(folder_path, "images/" "%s_p%s-%s.txt" % (path[:-4], page_num, img_xrefs[caption_num]))
+                        with open(caption_path, "w") as file:
                             caption =match.strip()
                             file.write(caption)
                         caption_num += 1
+
+
+
 
 if __name__ == "__main__":
     main()
