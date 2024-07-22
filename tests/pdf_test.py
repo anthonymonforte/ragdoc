da34be595c2c1e130d2bae49f1f9ee8a5a0873f8
Anthony Monforte <amonforte@gmail.com>
2024-07-22 09:43:22 -0700

    adding more unit tests for combine images; added test helper function to construct objects

--- tests/pdf_test.py
+++ tests/pdf_test.py
@@ -8,14 +8,43 @@
 
 import sys
 import os
+from typing import List
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import modules.pdf_module as pdf
 from modules.pdf_module import PdfImage, ImagePart
 
+def create_pdf_image_obj(image_bbox: List[float], page: int, xref: int):    
+    return PdfImage(image_bbox=image_bbox, image_page=page, caption=None, image_parts=[ImagePart(xref=xref, bbox=image_bbox)])
 
-def test_combine_images():
-    image_list = [PdfImage(image_bbox=[0,0,5,5], image_page=1, caption=None, image_parts=[ImagePart(xref=1, bbox=[0,0,5,5])])]
+def test_combine_images_should_not_combine_separate_images():
+    image_list = [create_pdf_image_obj([0,0,0,5], 1, 1),
+                  create_pdf_image_obj([0,10,0,15], 1, 2)]
+    result = pdf.combine_images(image_list)
+    assert len(result) == 2
+    assert len(result[0].image_parts) == 1
+    assert len(result[1].image_parts) == 1
+
+
+def test_combine_images_should_combine_image_parts():
+    image_list = [create_pdf_image_obj([0,0,0,5], 1, 1),
+                  create_pdf_image_obj([0,5,0,15], 1, 2)]
     result = pdf.combine_images(image_list)
     assert len(result) == 1
+    assert len(result[0].image_parts) == 2
+
+def test_combine_images_should_combine_image_parts_within_threshold():
+    image_list = [create_pdf_image_obj([0,0,0,5], 1, 1),
+                  create_pdf_image_obj([0,5 + pdf.IMAGE_OFFSET_THRESHOLD,0,15], 1, 2)]
+    result = pdf.combine_images(image_list)
+    assert len(result) == 1
+    assert len(result[0].image_parts) == 2
+
+def test_combine_images_should_not_combine_image_parts_outside_threshold():
+    image_list = [create_pdf_image_obj([0,0,0,5], 1, 1),
+                  create_pdf_image_obj([0,5 + pdf.IMAGE_OFFSET_THRESHOLD + .01,0,15], 1, 2)]
+    result = pdf.combine_images(image_list)
+    assert len(result) == 2
+    assert len(result[0].image_parts) == 1
+    assert len(result[1].image_parts) == 1
