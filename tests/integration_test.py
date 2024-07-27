2da275182a66c0734f4b2b2662fee6c29f86ca7c
Anthony Monforte <amonforte@gmail.com>
2024-07-27 10:47:59 -0700

    udating test names and assets to better describe what they're testing

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -17,8 +17,8 @@ def get_default_config():
     return pdf.Config(folder_path=os.path.dirname(__file__), caption_above_img=False, perform_audit=False)
 
 
-def test_extract_images_and_captions_from_pdf1():
-    doc_images = pdf.extract_images_and_captions_from_doc(os.path.join(os.path.dirname(__file__), "test_assets/pdfs/pdf1_test.pdf"), get_default_config())
+def test_extract_images_and_captions_from_pdf_1_image_bottom_caption_test():
+    doc_images = pdf.extract_images_and_captions_from_doc(os.path.join(os.path.dirname(__file__), "test_assets/pdfs/pdf_1_image_bottom_caption_test.pdf"), get_default_config())
 
     assert len(doc_images) == 1
     pdf_image = doc_images[0]
@@ -27,8 +27,8 @@ def test_extract_images_and_captions_from_pdf1():
     assert pdf_image.caption is not None
     assert pdf_image.caption.caption_text == "Fig. 1 This is a caption. \n"
 
-def test_extract_images_and_captions_from_pdf2():
-    doc_images = pdf.extract_images_and_captions_from_doc(os.path.join(os.path.dirname(__file__), "test_assets/pdfs/pdf2_test.pdf"), get_default_config())
+def test_extract_images_and_captions_from_pdf_2_images_one_bottom_caption_and_page_break_test():
+    doc_images = pdf.extract_images_and_captions_from_doc(os.path.join(os.path.dirname(__file__), "test_assets/pdfs/pdf_2_images_one_bottom_caption_and_page_break_test.pdf"), get_default_config())
 
     assert len(doc_images) == 2
 
@@ -41,10 +41,10 @@ def test_extract_images_and_captions_from_pdf2():
     assert pdf_image.caption is not None
     assert pdf_image.caption.caption_text == "Fig. 1 This is a caption, on the next page. \n"
 
-def test_extract_images_and_captions_from_pdf3():
+def test_extract_images_and_captions_from_pdf_2_images_top_captions_and_page_break():
     config = get_default_config()
     config.caption_above_img = True
-    doc_images = pdf.extract_images_and_captions_from_doc(os.path.join(os.path.dirname(__file__), "test_assets/pdfs/pdf3_test.pdf"), config)
+    doc_images = pdf.extract_images_and_captions_from_doc(os.path.join(os.path.dirname(__file__), "test_assets/pdfs/pdf_2_images_top_captions_and_page_break.pdf"), config)
 
     assert len(doc_images) == 2
 
