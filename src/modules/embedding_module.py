e31ecf3f6ee8df756d85b8cbdf412a37ba196fea
Anthony Monforte <amonforte@gmail.com>
2024-08-02 09:07:08 -0700

    added match statement to select embedding model

--- src/modules/embedding_module.py
+++ src/modules/embedding_module.py
@@ -12,9 +12,16 @@ from langchain_community.embeddings import OllamaEmbeddings
 
 class Embeddings:
 
-    def __init__(self, model: str) -> None:
+    def __init__(self, model: str, version: str) -> None:
         self.model = model
+        self.version = version
 
-    def embedding_function(self):
-        embeddings = OllamaEmbeddings(model="llama3:8b")
+    def get_embedding(self):
+        match self.model:
+            case "llama3":
+                return self.ollama_embedding_function()
+        return None
+
+    def ollama_embedding_function(self):
+        embeddings = OllamaEmbeddings(model=f"{self.model}:{self.version}") #e.g. llama3:8b
         return embeddings
